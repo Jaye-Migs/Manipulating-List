6df80cebaf5bf2102f7ae8367d066c9c3c587af0
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:54:45 +0800

    added game list.py

---
+++ game_list.py
@@ -0,0 +1,14 @@
+games = ['Portal', 'Half-Life', 'Doom', 'Overwatch', 'Fortnite', 'Celeste', 'Fez', 'Spelunky', 'Journey', 'Bastion', 'Limbo', 'Inside', 'Crysis', 'Destiny', 'Dishonored']
+print(games)
+
+print("7th game is: ", games[6])
+
+games[3] = 'Minecraft'
+print(games)
+
+del games[8]
+print(games)
+
+print(games[4:9])
+
+print("Last game is: ", games[-1])

00bc8a4f28f8eaf21e729900ea12814de1bae22c
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:17:18 +0800

    added color list.py

---
+++ color_list.py
@@ -0,0 +1,14 @@
+colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet', 'Magenta', 'Cyan', 'White', 'Brown', 'Black', 'Pink', 'Gray', 'Crimson']
+print(colors)
+
+print("10th color is: ", colors[9])
+
+colors[3] = 'Purple'
+print(colors)
+
+del colors[4]
+print(colors)
+
+print(colors[1:7])
+
+print("Last color is: ", colors[-1])

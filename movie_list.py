d98262529b994a8a1346c438d381be1d01ba48fa
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:47:55 +0800

    added movie list.py

---
+++ movie_list.py
@@ -0,0 +1,14 @@
+movie = ['Jaws', 'Titanic', 'Gladiator', '2012', 'Avatar', 'Frozen', 'Aliens', 'Braveheart', 'Psycho', 'Gravity', 'Unforgiven', 'Memento', 'Casablanca', 'Whiplash', 'Birdman', 'Interstellar', 'Rocky', 'Amelie', 'Predator', 'Kingsman']
+print(movie)
+
+print("12th movie is: ", movie[11])
+
+movie[14] = 'Inception'
+print(movie)
+
+del movie[17]
+print(movie)
+
+print(movie[7:12])
+
+print("Last movie is: ", movie[-1])

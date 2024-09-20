2dd1b84d721d5a73b3c8231bf6930b2a69da4cd7
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:05:48 +0800

    added animal list.py

---
+++ animal_list.py
@@ -0,0 +1,12 @@
+animals = ['Dog', 'Cat', 'Fish', 'Bird', 'Horse', 'Chicken', 'Cow', 'Goat', 'Deer', 'Capybara']
+print(animals)
+
+print("3rd animal is: ", animals[2])
+
+animals[5] = 'Elephant'
+print(animals)
+
+del animals[7]
+print(animals)
+
+print("Last animal is: ", animals[-1])

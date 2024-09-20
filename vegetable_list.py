4e72b5b8ee02664e692af88ba36f2b6add07efed
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:37:00 +0800

    added vegetable list.py

---
+++ vegetable_list.py
@@ -0,0 +1,12 @@
+vegetables = ['Carrot', 'Broccoli', 'Spinach', 'Bell Pepper', 'Cauliflower', 'Tomato', 'Zucchini']
+print(vegetables)
+
+print("5th vegetable is: ", vegetables[4])
+
+vegetables[2] = 'Spinach'
+print(vegetables)
+
+del vegetables[5]
+print(vegetables)
+
+print("Last vegetable is: ", vegetables[-1])

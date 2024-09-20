3dd5b490dc2f062197e5efc584fd4ea131cee5df
Mj Migueh <mjmigz31@gmail.com>
2024-09-20 23:47:52 +0800

    add student names.py

---
+++ student_name.py
@@ -0,0 +1,14 @@
+names = ['Mj', 'Michael', 'Angelo', 'Ram', 'Randy', 'Carl', 'Miguel', 'Gabriel', 'Adrian ', 'Justin', 'Christian', 'Sherwin', 'Danny', 'Paul', 'Victor', 'Louigi', 'Mario', 'Francis', 'Jitomi', 'Karlo']
+print(names)
+
+print("14th name is: ", names[14])
+
+names[11] = ('John')
+print(names)
+
+del names[9]
+print(names)
+
+print(names[1:5])
+
+print("Last name is: ", names[-1])

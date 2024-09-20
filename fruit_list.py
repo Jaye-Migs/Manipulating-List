78cc54d7d9dd9860f4b520fdab60129eccf68cc9
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 00:30:33 +0800

    added fruit list.py

---
+++ fruit_list.py
@@ -0,0 +1,14 @@
+fruits = ['Apple', 'Orange', 'Banana', 'Grapes', 'Pear', 'Lanzones', 'Rambutan', 'Coconut', 'Macapuno', 'Strawberry', 'Mangosteen', 'Guava']
+print(fruits)
+
+print("The 9th fruit is: ", fruits[8])
+
+fruits[1] = 'Mango'
+print(fruits)
+
+del fruits[9]
+print(fruits)
+
+print(fruits[3:6])
+
+print("Last fruit is: ", fruits[-1])

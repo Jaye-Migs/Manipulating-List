ea3426e7101547ce83b12297c87d7e58b819dcad
Mj Migueh <mjmigz31@gmail.com>
2024-09-21 01:39:04 +0800

    added planet list.py

---
+++ planet_list.py
@@ -0,0 +1,13 @@
+planets = ['Venus', 'Mercury', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets)
+
+print("3rd planet is: ", planets[2])
+
+planets[6] = 'Pluto'
+print(planets)
+
+del planets[3]
+print(planets)
+
+print("Last planet is: ", planets[-1])
+

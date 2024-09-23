7680834f7d8bb8b9bd13c29d8a2ad5481b76efec
Mj Migueh <mjmigz31@gmail.com>
2024-09-23 23:41:42 +0800

    add language list.py

---
+++ language_list.py
@@ -0,0 +1,14 @@
+programming_language = ['HTML', 'JavaScript', 'Java', 'C++', 'C#', 'Ruby', 'Swift', 'Go', 'PHP', 'TypeScript', 'Kotlin', 'Rust', 'Perl', 'R']
+print(programming_language)
+
+print("The 11th language is: ", programming_language[10])
+
+programming_language[8] = "Python"
+print(programming_language)
+
+del programming_language[12]
+print(programming_language)
+
+print(programming_language[4:13])
+
+print("The last language is: ", programming_language[-1])

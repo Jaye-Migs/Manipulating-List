d7ef8059416256b41d2c8de922180c9c1f4f5946
Mj Migueh <mjmigz31@gmail.com>
2024-09-23 23:59:27 +0800

    add spoken language list.py

---
+++ language_spoken_list.py
@@ -0,0 +1,14 @@
+language = ['English', 'Tagalog', 'Mandarin', 'Hindi', 'Arabic', 'Portuguese', 'Bengali', 'Russian', 'Japanese', 'Punjabi', 'German', 'Javanese', 'Wu', 'French', 'Korean', 'Vietnamese', 'Tamil', 'Italian', 'Urdu', 'Turkish']
+print(language)
+
+print("The 13th language is: ", language[12])
+
+language[9] = "Spanish"
+print(language)
+
+del language[15]
+print(language)
+
+print(language[5:12])
+
+print("The last language is: ", language[-1])

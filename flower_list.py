4e42bc9c1796773cf812d8ff482a4c7d4c985000
Mj Migueh <mjmigz31@gmail.com>
2024-09-23 23:49:42 +0800

    add flower list.py

---
+++ flower_list.py
@@ -0,0 +1,12 @@
+flower = ['Tulip', 'Lily', 'Orchid', 'Daisy', 'Sunflower', 'Daffodil', 'Hydrangea', 'Marigold', 'Chrysanthemum', 'Iris']
+print(flower)
+
+print("The 5th flower is: ", flower[4])
+
+flower[7] = "Rose"
+print(flower)
+
+del flower[1]
+print(flower)
+
+print("The last flower is: ", flower[-1])

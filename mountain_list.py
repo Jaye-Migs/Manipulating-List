c5fe8d9f558c26ae5138f93987c5b42e6d9160e2
Mj Migueh <mjmigz31@gmail.com>
2024-09-24 00:08:11 +0800

    add mountain list.py

---
+++ mountain_list.py
@@ -0,0 +1,12 @@
+mountain = ['Apo', 'K2', 'Kangchenjunga', 'Lhotse', 'Makalu', 'Cho Oyu', 'Dhaulagiri', 'Manaslu']
+print(mountain)
+
+print("The 5th mountain is: ", mountain[4])
+
+mountain[2] = "Everest"
+print(mountain)
+
+del mountain[5]
+print(mountain)
+
+print("The last mountain is: ", mountain[-1])

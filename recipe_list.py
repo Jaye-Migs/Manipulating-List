e120743bee62daf884fdf39d8e8a10e15b2fbf16
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:31:29 +0800

    add recipe list.py

---
+++ recipe_list.py
@@ -0,0 +1,14 @@
+recipe = ['Spaghetti Carbonara', 'Chicken Tikka Masala', 'Beef Stroganoff', 'Pad Thai', 'Sushi', 'Biryani', 'Chili Con Carne', 'Ratatouille', 'Fried Rice', 'Moussaka', 'Tacos', 'Clam Chowder', 'Goulash', 'Peking Duck', 'Quiche']
+print(recipe)
+
+print("12th recipe is: ", recipe[11])
+
+recipe[8] = 'Lasagna'
+print(recipe)
+
+del recipe[10]
+print(recipe)
+
+print(recipe[4:11])
+
+print("Last recipe is: ", recipe[-1])

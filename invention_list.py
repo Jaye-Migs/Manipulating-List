7684ac04432b5de60341e5a1427b5598bddbe446
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:27:31 +0800

    add invention list.py

---
+++ invention_list.py
@@ -0,0 +1,12 @@
+invention = ['Wheel', 'Telephone', 'Airplane', 'Printing Press', 'Internet', 'Automobile', 'Refrigerator', 'Computer']
+print(invention)
+
+print("6th invention is: ", invention[5])
+
+invention[1] = 'Lightbulb'
+print(invention)
+
+del invention[4]
+print(invention)
+
+print(" is: ", invention[-1])

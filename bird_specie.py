9e686f2831c3405416228b24aa51b370d62b05c7
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:14:48 +0800

    add bird specie list.py

---
+++ bird_specie.py
@@ -0,0 +1,14 @@
+specie = ['Peregrine Falcon', 'Hummingbird', 'Sparrow', 'Cardinal', 'Blue Jay', 'Penguin', 'Owl', 'Parrot', 'Flamingo', 'Woodpecker', 'Swallow', 'Canary']
+print(specie)
+
+print("9th bird specie is: ", specie[8])
+
+specie[5] = 'Eagle'
+print(specie)
+
+del specie[7]
+print(specie)
+
+print(specie[2:8])
+
+print("Last bird specie is: ", specie[-1])

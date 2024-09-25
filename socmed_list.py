4917fb894cae0d8ea17d791f36c4d347c172e706
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:13:50 +0800

    add socmed list.py

---
+++ socmed_list.py
@@ -0,0 +1,12 @@
+platform = ['Facebook', 'Twitter', 'LinkedIn', 'Snapchat', 'TikTok', 'Pinterest', 'Reddit', 'YouTube', 'Tumblr', 'WhatsApp']
+print(platform)
+
+print("5th socmed is: ", platform[4])
+
+platform[2] = 'Instagram'
+print(platform)
+
+del platform[7]
+print(platform)
+
+print("Last socmed is: ", platform[-1])

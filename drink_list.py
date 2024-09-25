dbed0b08ec58257c9304d699cdf6c59e109e5351
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:34:39 +0800

    add drink list.py

---
+++ drink_list.py
@@ -0,0 +1,12 @@
+drink = ['Tea', 'Lemonade', 'Soda', 'Iced Tea', 'Hot Chocolate', 'Juice', 'Beer', 'Wine']
+print(drink)
+
+print("4th drink is: ", drink[3])
+
+drink[2] = 'Coffee'
+print(drink)
+
+del drink[6]
+print(drink)
+
+print("Last drink is: ", drink[-1])

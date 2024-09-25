530a25b8afd4d4fbdb1cbf7dee575da1748a57e2
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:03:07 +0800

    add habitat list.py

---
+++ habitat_list.py
@@ -0,0 +1,12 @@
+habitat = ['Rainforest', 'Desert', 'Grassland', 'Wetlands', 'Tundra', 'Ocean', 'Coral Reef', 'Mountains']
+print(habitat)
+
+print("5th habitat is: ", habitat[4])
+
+habitat[2] = 'Savanna'
+print(habitat)
+
+del habitat[5]
+print(habitat)
+
+print("Last habitat is: ", habitat[-1])

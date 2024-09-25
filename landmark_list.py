f65d3f9979321c93203dffb892b3fb1b104c6128
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:48:52 +0800

    add landmark list.py

---
+++ landmark_list.py
@@ -0,0 +1,12 @@
+landmark = ['Mount Everest', 'Great Barrier Reef', 'Niagara Falls', 'Yellowstone National Park', 'Sahara Desert', 'Victoria Falls', 'Yosemite National Park', 'Stonehenge', 'Machu Picchu', 'Galapagos Islands']
+print(landmark)
+
+print("8th landmark is: ", landmark[7])
+
+landmark[4] = 'Grand Canyon'
+print(landmark)
+
+del landmark[8]
+print(landmark)
+
+print("Last landmark is: ", landmark[-1])

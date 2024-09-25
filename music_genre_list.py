22c470e42481bd90fb8004d5149231e1830b24a6
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:24:27 +0800

    add music genre list.py

---
+++ music_genre_list.py
@@ -0,0 +1,14 @@
+genre = ['Rock', 'Pop', 'Hip-Hop', 'Classical', 'Country', 'Reggae', 'Blues', 'Electronic', 'Metal', 'Folk', 'R&B', 'Punk', 'Disco', 'Gospel', 'Indie']
+print(genre)
+
+print("10th genre is: ", genre[9])
+
+genre[4] = 'Jazz'
+print(genre)
+
+del genre[6]
+print(genre)
+
+print(genre[2:9])
+
+print("Last genre is: ", genre[-1])

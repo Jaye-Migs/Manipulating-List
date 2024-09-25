1104f77fce049cca85cd60ac8cf2a615779fd29b
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 01:17:24 +0800

    add world wonder list.py

---
+++ world_wonder_list.py
@@ -0,0 +1,12 @@
+wwonders = ['Petra', 'Christ the Redeemer', 'Rizal Statute', 'Chichen Itza', 'Roman Colosseum', 'Taj Mahal', 'Machu Picchu']
+print(wwonders)
+
+print("4th world wonder is: ", wwonders[3])
+
+wwonders[2] = 'Great Wall of China'
+print(wwonders)
+
+del wwonders[4]
+print(wwonders)
+
+print("Last world wonder is: ", wwonders[-1])

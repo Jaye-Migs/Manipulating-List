3411fa5afa359b3280f05fe0056ed8c868c522aa
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:34:35 +0800

    add holiday list.py

---
+++ holiday_list.py
@@ -0,0 +1,14 @@
+holiday = ['New Years Day', 'Easter', 'Thanksgiving', 'Halloween', 'Valentines Day', 'Independence Day', 'Labor Day', 'Memorial Day', 'Hanukkah', 'Diwali', 'Thanksgiving', 'Chinese New Year']
+print(holiday)
+
+print("9th holiday is: ", holiday[8])
+
+holiday[2] = 'Christmas'
+print(holiday)
+
+del holiday[10]
+print(holiday)
+
+print(holiday[1:8])
+
+print("Last holiday is: ", holiday[-1])

24a887e204297a8a5284914aeeebcd99bfd8b9f1
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:29:15 +0800

    add building list.py

---
+++ building_list.py
@@ -0,0 +1,12 @@
+building = ['Colosseum', 'Taj Mahal', 'Great Wall of China', 'Burj Khalifa', 'Sydney Opera House', 'Statue of Liberty', 'Big Ben', 'Machu Picchu']
+print(building)
+
+print("5th building is: ", building[4])
+
+building[1] = 'Eiffel Tower'
+print(building)
+
+del building[5]
+print(building)
+
+print("Last building is: ", building[-1])

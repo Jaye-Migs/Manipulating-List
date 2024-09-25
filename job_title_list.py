417a5c58b006a0feba72fe7083c579c5639211dd
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:07:39 +0800

    add job title list.py

---
+++ job_title_list.py
@@ -0,0 +1,12 @@
+job = ['Data Scientist', 'Project Manager', 'Marketing Specialist', 'Graphic Designer', 'Sales Representative', 'Human Resources Manager', 'Financial Analyst', 'Content Writer', 'Web Developer', 'Product Manager']
+print(job)
+
+print("6th job title is: ", job[5])
+
+job[4] = 'Software Engineer'
+print(job)
+
+del job[8]
+print(job)
+
+print("Last job title is: ", job[-1])

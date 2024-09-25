1154707482ce9803c74793563c1f5108ee96074d
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:05:30 +0800

    add author list.py

---
+++ author_list.py
@@ -0,0 +1,14 @@
+author = ['William Shakespeare', 'Jane Austen', 'Tolkien', 'George Orwell', 'F. Scott Fitzgerald', 'Herman Melville', 'J.K. Rowling', 'Leo Tolstoy', 'Charles Dickens', 'Virginia Woolf', 'Gabriel Garcia Marquez', 'Ernest Hemingway']
+print(author)
+
+print("The 6th author is: ", author[5])
+
+author[3] = 'Mark Twain'
+print(author)
+
+del author[9]
+print(author)
+
+print (author[2:9])
+
+print("The last author is: ", author[-1])

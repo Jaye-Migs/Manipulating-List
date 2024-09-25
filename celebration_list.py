20f10dfbf5d938d9d1da1b4673c6ab343ce4254b
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:42:33 +0800

    add celebration list.py

---
+++ celebration_list.py
@@ -0,0 +1,12 @@
+festival = ['Holi', 'Eid al-Fitr', 'Oktoberfest', 'Mardi Gras', 'Carnival', 'Christmas', 'Diwali', 'Baisakhi', 'Hanukkah', 'Lunar New Year',]
+print(festival)
+
+print("5th festival is: ", festival[4])
+
+festival[2] = 'Thanksgiving'
+print(festival)
+
+del festival[6]
+print(festival)
+
+print("Last festival is: ", festival[-1])

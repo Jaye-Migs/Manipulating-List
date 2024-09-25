359c34772acc5eb37bdec6f4dfc63ba4f626cf6f
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:10:20 +0800

    add company list.py

---
+++ company_list.py
@@ -0,0 +1,12 @@
+company = ['Microsoft', 'Google', 'Amazon', 'Facebook', 'Tesla', 'Netflix', 'Samsung', 'IBM', 'Intel', 'Sony']
+print(company)
+
+print("7th company is: ", company[6])
+
+company[4] = 'Apple'
+print(company)
+
+del company[7]
+print(company)
+
+print("Last company is: ", company[-1])

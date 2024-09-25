684f8e14e0aec8543b12c7e588f47206bc068a37
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:14:06 +0800

    add pokemon list.py

---
+++ pokemon_list.py
@@ -0,0 +1,14 @@
+pokemon = ['Charizard', 'Bulbasaur', 'Squirtle', 'Jigglypuff', 'Eevee', 'Gengar', 'Mewtwo', 'Snorlax', 'Lapras', 'Dragonite', 'Lucario', 'Gardevoir', 'Togepi', 'Arcanine', 'Scyther']
+print(pokemon)
+
+print("9th pokemon is: ", pokemon[8])
+
+pokemon[11] = 'Pikachu'
+print(pokemon)
+
+del pokemon[9]
+print(pokemon)
+
+print(pokemon[3:8])
+
+print(" is: ", pokemon[-1])

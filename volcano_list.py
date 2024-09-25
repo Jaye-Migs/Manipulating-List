3ce144e6d6835938d246a0ea15990d9220e34401
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 01:06:35 +0800

    volcano list.py

---
+++ volcano_list.py
@@ -0,0 +1,12 @@
+volcano = ['Kilimanjaro', 'Mount Fuji', 'Krakatoa', 'Mount St. Helens', 'Mauna Loa', 'Mount Etna', 'Mount Rainier']
+print(volcano)
+
+print("4th volcano is: ", volcano[3])
+
+volcano[2] = 'Mount Vesuvius'
+print(volcano)
+
+del volcano[4]
+print(volcano)
+
+print("Last volcano is: ", volcano[-1])

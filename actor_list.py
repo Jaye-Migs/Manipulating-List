fccfd64f1e12153912206903c48709144d70091f
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:32:51 +0800

    add actor list.py

---
+++ actor_list.py
@@ -0,0 +1,12 @@
+actor = ['Robert Downey Jr.', 'Meryl Streep', 'Tom Hanks', 'Angelina Jolie', 'Denzel Washington', 'Scarlett Johansson', 'Johnny Depp', 'Brad Pitt', 'Natalie Portman', 'Will Smith']
+print(actor)
+
+print("7th actor is: ", actor[6])
+
+actor[2] = 'Leonardo DiCaprio'
+print(actor)
+
+del actor[7]
+print(actor)
+
+print("Last actor is: ", actor[-1])

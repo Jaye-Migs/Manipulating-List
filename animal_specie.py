3df8be8e8e97ffc83d5810216e92ebd79bd62e20
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:17:40 +0800

    add animal specie.py

---
+++ animal_specie.py
@@ -0,0 +1,12 @@
+specie = ['Lion', 'Elephant', 'Kangaroo', 'Penguin', 'Dolphin', 'Tiger', 'Zebra']
+print(specie)
+
+print("is: ", specie[1])
+
+specie[4] = 'Gorilla'
+print(specie)
+
+del specie[5]
+print(specie)
+
+print(" is: ", specie[-1])

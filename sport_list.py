4cb779cdb380e56ee36fe6d6653e20acf0cbddcd
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:18:56 +0800

    add sport list.py

---
+++ sport_list.py
@@ -0,0 +1,12 @@
+sport = ['Soccer', 'Cricket', 'Tennis', 'Baseball', 'Rugby', 'Hockey', 'Golf', 'American Football', 'Volleyball', 'Table Tennis']
+print(sport)
+
+print("6th sport is: ", sport[5])
+
+sport[3] = 'Basketball'
+print(sport)
+
+del sport[8]
+print(sport)
+
+print("Last sport is: ", sport[-1])

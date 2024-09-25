7cb9f44cd61e49426b68adbc85956210b7671808
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:02:39 +0800

    add festival list.py

---
+++ festival_list.py
@@ -0,0 +1,14 @@
+festival = ['Holi', 'Eid al-Fitr', 'Oktoberfest', 'Mardi Gras', 'Carnival', 'Christmas', 'Thanksgiving', 'Baisakhi', 'Hanukkah', 'Lunar New Year', 'Coachella', 'Gion Matsuri', 'Songkran', 'Glastonbury Festival', 'Burning Man']
+print(festival)
+
+print("7th festival is: ", festival[6])
+
+festival[10] = 'Diwali'
+print(festival)
+
+del festival[8]
+print(festival)
+
+print(festival[4:13])
+
+print("Last festival is: ", festival[-1])

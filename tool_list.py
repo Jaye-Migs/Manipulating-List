fec2a3c4d5d949d87745c36add66a725549bcf29
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:42:55 +0800

    add tool list.py

---
+++ tool_list.py
@@ -0,0 +1,12 @@
+tool = ['Saw', 'Chisel', 'Square', 'Level', 'Tape Measure', 'Drill', 'Screwdriver', 'Clamps', 'Router', 'Sandpaper']
+print(tool)
+
+print("4th tool is: ", tool[3])
+
+tool[6] = 'Hammer'
+print(tool)
+
+del tool[4]
+print(tool)
+
+print("Last tool is: ", tool[-1])

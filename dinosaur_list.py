2ed4ee9cb069d2f83a061ffceda8cd56ea267e64
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:45:52 +0800

    add dinosaur list.py

---
+++ dinosaur_list.py
@@ -0,0 +1,12 @@
+dinosaur = ['Velociraptor', 'Triceratops', 'Brachiosaurus', 'Stegosaurus', 'Ankylosaurus', 'Spinosaurus', 'Diplodocus', 'Allosaurus']
+print(dinosaur)
+
+print("4th dinosaur is: ", dinosaur[3])
+
+dinosaur[5] = 'Tyrannosaurus Rex'
+print(dinosaur)
+
+del dinosaur[6]
+print(dinosaur)
+
+print("Last Dinosaur is: ", dinosaur[-1])

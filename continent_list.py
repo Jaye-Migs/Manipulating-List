8f4fa6f6be3556d23aea82d7d6f410aec62a0bf5
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:06:01 +0800

    add continent list.py

---
+++ continent_list.py
@@ -0,0 +1,12 @@
+continent = ['Asia', 'Europe', 'North America', 'South America', 'Antarctica', 'Australia', 'Eurasia']
+print(continent)
+
+print("4th continent is: ", continent[3])
+
+continent[1] = 'Africa'
+print(continent)
+
+del continent[5]
+print(continent)
+
+print("Last continent is: ", continent[-1])

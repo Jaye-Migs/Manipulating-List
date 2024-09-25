fad5aa524a2239b719a05b3bc45d16742fdb2cb5
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 01:09:20 +0800

    add waterfall list.py

---
+++ waterfall_list.py
@@ -0,0 +1,12 @@
+waterfall = ['Angel Falls', 'Iguazu Falls', 'Victoria Falls', 'Yosemite Falls', 'Plitvice Falls', 'Sutherland Falls', 'Kaieteur Falls', 'Multnomah Falls', 'Ban Gioc-Detian Falls', 'Tugela Falls']
+print(waterfall)
+
+print("8th waterfall is: ", waterfall[7])
+
+waterfall[4] = 'Niagara Falls'
+print(waterfall)
+
+del waterfall[8]
+print(waterfall)
+
+print("Last waterfall is: ", waterfall[-1])

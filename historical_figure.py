36a906a3c2349bd92e9d9bed287ae92db595c10b
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 22:22:52 +0800

    add historical figures.py

---
+++ historical_figure.py
@@ -0,0 +1,12 @@
+figure = ['Julius Caesar', 'Cleopatra', 'Albert Einstein', 'Mahatma Gandhi', 'Martin Luther King Jr.', 'Marie Curie', 'Leonardo da Vinci', 'Winston Churchill', 'Abraham Lincoln', 'Queen Elizabeth I']
+print(figure)
+
+print("8th figure is: ", figure[7])
+
+figure[3] = 'Nelson Mandela'
+print(figure)
+
+del figure[6]
+print(figure)
+
+print("Last figure is: ", figure[-1])

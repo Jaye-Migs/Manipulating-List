6166f6a07463449076e5e31c5d98b5802770efcd
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:58:51 +0800

    add tech invention list.py

---
+++ tech_invention_list.py
@@ -0,0 +1,14 @@
+invention = ['Smartwatches', 'Drones', '3D Printers', 'Virtual Reality Headsets', 'Electric Cars', 'Wearable Fitness Trackers', 'Artificial Intelligence', 'Home Automation Systems', 'Blockchain Technology', 'Augmented Reality', 'Self-Driving Cars', 'Wireless Chargers', 'Voice Assistants', 'Telehealth Solutions', 'Biometric Security Systems']
+print(invention)
+
+print("11th invention is: ", invention[10])
+
+invention[7] = 'Smartphone'
+print(invention)
+
+del invention[13]
+print(invention)
+
+print(invention[4:11])
+
+print(" is: ", invention[-1])

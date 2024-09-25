e2b46f997e1106a98f27b3e3e24049f50cded075
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:38:37 +0800

    add subject list.py

---
+++ subject_list.py
@@ -0,0 +1,14 @@
+subject = ['English', 'Science', 'History', 'Geography', 'Art', 'Physical Education', 'Music', 'Biology', 'Chemistry', 'Physics', 'Computer Science', 'Foreign Language']
+print(subject)
+
+print("6th subject is: ", subject[5])
+
+subject[7] = 'Mathematics'
+print(subject)
+
+del subject[3]
+print(subject)
+
+print(subject[1:6])
+
+print("Last subject is: ", subject[-1])

812bba0c65ce4da3ad0d16ed423a7c95980f7299
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 01:03:31 +0800

    add river list.py

---
+++ river_list.py
@@ -0,0 +1,12 @@
+river = ['Amazon River', 'Yangtze River', 'Mississippi River', 'Ganges River', 'Danube River', 'Thames River', 'Volga River', 'Zambezi River', 'Rio Grande River', 'Mekong River']
+print(river)
+
+print("6th river is: ", river[5])
+
+river[7] = 'Nile River'
+print(river)
+
+del river[8]
+print(river)
+
+print("Last river is: ", river[-1])

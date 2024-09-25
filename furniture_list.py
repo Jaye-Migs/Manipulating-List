8b92154cf0f82b2af8ebd7500b26ce92f3bac48b
Mj Migueh <mjmigz31@gmail.com>
2024-09-25 23:10:28 +0800

    add furniture list.py

---
+++ furniture_list.py
@@ -0,0 +1,12 @@
+furniture = ['Chair', 'Table', 'Bed', 'Cabinet', 'Desk', 'Dresser', 'Bookshelf', 'Coffee Table', 'Nightstand', 'Ottoman', 'Wardrobe', 'Bench']
+print(furniture)
+
+print("8th furniture is: ", furniture[7])
+
+furniture[4] = 'Sofa'
+print(furniture)
+
+del furniture[9]
+print(furniture)
+
+print("Last furniture is: ", furniture[-1])

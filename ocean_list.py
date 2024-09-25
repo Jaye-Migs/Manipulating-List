08a1829513ea8597d9f3477c20243aa23ab14cc2
Mj Migueh <mjmigz31@gmail.com>
2024-09-26 00:10:56 +0800

    add ocean list.py

---
+++ ocean_list.py
@@ -0,0 +1,12 @@
+ocean = ['Atlantic Ocean', 'Indian Ocean', 'Southern Ocean', 'Arctic Ocean', 'Mediterranean Sea']
+print(ocean)
+
+print("3rd ocean is: ", ocean[2])
+
+ocean[1] = 'Pacific Ocean'
+print(ocean)
+
+del ocean[3]
+print(ocean)
+
+print("Last ocean is: ", ocean[-1])
